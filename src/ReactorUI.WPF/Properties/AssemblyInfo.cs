e4a263f72b5033a14480e905615ee81d74b0b1c0
Adolfo Marinucci <adospace@msn.com>
2019-11-23 14:41:19 +0100

    Converting WPF library to .net core

--- src/ReactorUI.WPF/Properties/AssemblyInfo.cs
+++ src/ReactorUI.WPF/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -7,14 +7,14 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ReactorUI.WPF")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ReactorUI.WPF")]
-[assembly: AssemblyCopyright("Copyright © 2018 Marinucci Adolfo")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+//[assembly: AssemblyTitle("ReactorUI.WPF")]
+//[assembly: AssemblyDescription("")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyCompany("")]
+//[assembly: AssemblyProduct("ReactorUI.WPF")]
+//[assembly: AssemblyCopyright("Copyright © 2018 Marinucci Adolfo")]
+//[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+//[assembly: AssemblyVersion("0.0.1.0")]
+//[assembly: AssemblyFileVersion("0.0.1.0")]
